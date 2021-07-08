dea2c648cf3b661ba1f89a3e4c3defe6a3bccfa7
daoducnha <daoducnha29493@gmail.com>
2021-07-09 00:13:42 +0700

    update code chap 2

--- taco-cloud/src/main/java/sia/tacocloud/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/sia/tacocloud/tacos/web/DesignTacoController.java
@@ -4,11 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import sia.tacocloud.tacos.Ingredient;
+import sia.tacocloud.tacos.Taco;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import static sia.tacocloud.tacos.Ingredient.Type;
 
 @Slf4j
@@ -16,7 +20,7 @@ import static sia.tacocloud.tacos.Ingredient.Type;
 @RequestMapping("/design")
 public class DesignTacoController {
     @GetMapping
-    public String showDesignForm(Model model){
+    public String showDesignForm(Model model) {
         List<Ingredient> ingredients = Arrays.asList(
                 new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
                 new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
@@ -30,13 +34,25 @@ public class DesignTacoController {
                 new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
         );
 
-        Type[]  types = Ingredient.Type.values();
-        for(Type type: types) {
+        Type[] types = Ingredient.Type.values();
+        for (Type type : types) {
             model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
         }
         model.addAttribute("design", new Taco());
         return "design";
     }
 
+    @PostMapping
+    public String processDesign(Taco design) {
+        log.info("Processing design: " + design);
+        return "redirect:/order/current";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
 
 }
